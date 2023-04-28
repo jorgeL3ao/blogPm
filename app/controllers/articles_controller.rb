1d015b52ab636022b944ebb18bc8502b99c5de89
jorgeL3ao <jorgesalgado1190@gmail.com>
2023-04-28 11:03:07 -0300

    Finalizano o crud Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,51 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[edit update show destory]
+
   def index
     @articles = Article.all
   end
 
   def show
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save 
+      redirect_to @article 
+    else
+      render :new 
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article 
+    else
+      render :edit
+    end
+  end
+
+  def destroy   
+    @article.destroy
+    
+    redirect_to root_path
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:titlle, :body)
+  end
+
+  def set_article
     @article = Article.find(params[:id])
   end
 end
